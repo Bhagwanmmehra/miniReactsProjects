5a9b9ffff10bef6676689f203a600afa244be0f9
bhagwanmmehra <bhagwanmehra04@gmail.com>
2024-12-21 22:47:56 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Card from './assets/Card'
+
+const appStyle={
+  display:"flex",
+  alignItems:"center",
+  justifyContent:"center",
+  height:"100vh"
+}
+
+
+function App() {
+  
+
+  return (
+    <div style={appStyle}>
+<Card/>
+
+</div>
+  )
+}
+
+export default App
